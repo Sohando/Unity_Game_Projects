1cb6e021be72e9829b1206b5194036860394d1c0
Sohando <sohandoabernahin1@gmail.com>
2020-10-04 13:44:12 +0600

    mad-birds/green-bird/script

---
+++ MAD BIRDS - YTD/Assets/Bird.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private void OnMouseDown() 
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+    private void OnMouseUp() 
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
