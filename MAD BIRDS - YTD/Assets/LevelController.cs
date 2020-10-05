8084dea98d2d7aedc57f1161fca8860082eb11aa
Sohando <sohandoabernahin1@gmail.com>
2020-10-05 12:13:08 +0600

    Done!

---
+++ MAD BIRDS - YTD/Assets/LevelController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private static int _nextLevelIndex = 1;
+    private Enemy[] _enemies;
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+    void Update()
+    {        
+        foreach(Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+            {
+                return;
+            }
+        }
+        _nextLevelIndex++;
+        string nextlevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextlevelName);
+    }
+}
